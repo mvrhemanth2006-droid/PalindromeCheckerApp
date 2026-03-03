44915819197fc4f31f99e4d7bba5be12c6873909
mvrhemanth2006-droid <mvrhemanth2006@gmail.com>
2026-03-03 11:05:38 +0530

    Added UseCase13 - <clear description of functionality>

--- PalindromeChecker/src/Main.java
+++ PalindromeChecker/src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 import java.util.Stack;
-import java.util.Deque;
-import java.util.ArrayDeque;
 
-public class UseCase12PalindromeCheckerApp {
+public class UseCase13PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
@@ -12,36 +10,21 @@ public class UseCase12PalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        System.out.println("Choose Strategy:");
-        System.out.println("1 - Stack Strategy");
-        System.out.println("2 - Deque Strategy");
-        System.out.print("Enter choice: ");
-        int choice = scanner.nextInt();
+        long startTime = System.nanoTime();
 
-        PalindromeStrategy strategy;
+        boolean result = checkUsingStack(input);
 
-        if (choice == 1) {
-            strategy = new StackStrategy();
-        } else {
-            strategy = new DequeStrategy();
-        }
-
-        boolean result = strategy.check(input);
+        long endTime = System.nanoTime();
+        long executionTime = endTime - startTime;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
 
         scanner.close();
     }
-}
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
+    private static boolean checkUsingStack(String input) {
 
         Stack<Character> stack = new Stack<>();
 
@@ -59,22 +42,3 @@ class StackStrategy implements PalindromeStrategy {
     }
 }
 
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
