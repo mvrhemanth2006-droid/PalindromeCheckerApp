64e6ec2c06b4dc64b5e016df96d3d4a11a467407
sameer-10317 <ijsameersha280605@gmail.com>
2026-03-03 10:08:22 +0530

    Initial commit - main file

---
+++ PalindromeChecker/src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Palindrome checker management ");
+        System.out.println("Application name ");
+        System.out.println("welcome to the palindrome checker system");
+        System.out.println("System initialized successfully");
+
+    }
+}
